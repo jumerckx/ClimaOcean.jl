364c2a677c091aaf91bfad1c628a9bd0680e3790
jumerckx <31353884+jumerckx@users.noreply.github.com>
2025-03-19 09:58:46 -0500

    debug fix

--- src/OceanSeaIceModels/InterfaceComputations/interpolate_atmospheric_state.jl
+++ src/OceanSeaIceModels/InterfaceComputations/interpolate_atmospheric_state.jl
@@ -66,8 +66,8 @@ function interpolate_atmosphere_state!(interfaces, atmosphere::PrescribedAtmosph
     times = ua.times
     time_indexing = ua.time_indexing
     t = clock.time
-    @assert time isa AbstractRange
     Core.println("time is a $(typeof(t))")
+    @assert time isa AbstractRange
     time_interpolator = TimeInterpolator(ua.time_indexing, times, clock.time)
     
     launch!(arch, grid, kernel_parameters,
